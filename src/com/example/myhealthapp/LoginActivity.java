76c47a1be9ec4b353b369f79dc5941ea597b1577
Stefan <stefan.strijker@outlook.com>
2013-09-24 12:54:21 +0200

    Added requestHandler, and totally redid the generated loginActivity

--- src/com/example/myhealthapp/LoginActivity.java
+++ src/com/example/myhealthapp/LoginActivity.java
@@ -1,53 +1,33 @@
 package com.example.myhealthapp;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.annotation.TargetApi;
+import java.io.UnsupportedEncodingException;
+import java.util.Map;
 import android.app.Activity;
 import android.content.Intent;
-import android.os.AsyncTask;
-import android.os.Build;
 import android.os.Bundle;
-import android.text.TextUtils;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Activity which displays a login screen to the user, offering registration as
  * well.
  */
 public class LoginActivity extends Activity {
-	/**
-	 * A dummy authentication store containing known user names and passwords.
-	 * TODO: remove after connecting to a real authentication system.
-	 */
-	private static final String[] DUMMY_CREDENTIALS = new String[] {
-			"foo@example.com:hello", "bar@example.com:world" };
 
-	/**
-	 * The default email to populate the email field with.
-	 */
-	public static final String EXTRA_EMAIL = "com.example.android.authenticatordemo.extra.EMAIL";
-
-	/**
-	 * Keep track of the login task to ensure we can cancel it if requested.
-	 */
-	private UserLoginTask mAuthTask = null;
-
-	// Values for email and password at the time of the login attempt.
-	private String mEmail;
-	private String mPassword;
+	// Values for username and password at the time of the login attempt.
+	String url;
+	String result;
 
 	// UI references.
-	private EditText mEmailView;
+	private EditText mUsernameView;
 	private EditText mPasswordView;
-	private View mLoginFormView;
-	private View mLoginStatusView;
-	private TextView mLoginStatusMessageView;
+	private EditText mHostView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -56,33 +36,29 @@ public class LoginActivity extends Activity {
 		setContentView(R.layout.activity_login);
 
 		// Set up the login form.
-		mEmail = getIntent().getStringExtra(EXTRA_EMAIL);
-		mEmailView = (EditText) findViewById(R.id.username);
-		mEmailView.setText(mEmail);
-
+		mUsernameView = (EditText) findViewById(R.id.username);
 		mPasswordView = (EditText) findViewById(R.id.password);
+		mHostView = (EditText) findViewById(R.id.host);
+
 		mPasswordView
 				.setOnEditorActionListener(new TextView.OnEditorActionListener() {
 					@Override
 					public boolean onEditorAction(TextView textView, int id,
 							KeyEvent keyEvent) {
 						if (id == R.id.login || id == EditorInfo.IME_NULL) {
-							attemptLogin();
+							attemptLogin(textView);
 							return true;
 						}
 						return false;
 					}
 				});
 
-		mLoginFormView = findViewById(R.id.login_form);
-		mLoginStatusView = findViewById(R.id.login_status);
-		mLoginStatusMessageView = (TextView) findViewById(R.id.login_status_message);
 
 		findViewById(R.id.sign_in_button).setOnClickListener(
 				new View.OnClickListener() {
 					@Override
 					public void onClick(View view) {
-						attemptLogin();
+						attemptLogin(view);
 					}
 				});
 	}
@@ -94,152 +70,76 @@ public class LoginActivity extends Activity {
 		return true;
 	}
 
-	/**
-	 * Attempts to sign in or register the account specified by the login form.
-	 * If there are form errors (invalid email, missing fields, etc.), the
-	 * errors are presented and no actual login attempt is made.
-	 */
-	public void attemptLogin() {
-		if (mAuthTask != null) {
-			return;
+	public void attemptLogin(View v) {
+		String username = mUsernameView.getText().toString();
+		String password = mPasswordView.getText().toString();
+		String host = mHostView.getText().toString();
+		if(!(host.equals(""))){
+			requestHandler.host = host;
+		}		
+		String url = "";
+		String name = "user";
+		String method = "login";
+
+		requestHandler handler = new requestHandler();
+
+		StringBuffer urlbuffer = new StringBuffer(requestHandler.host);
+
+		try {
+			username = handler.encode(username);
+			password = handler.encode(password);
+			name = handler.encode(name.toString());
+			method = handler.encode(method);
+
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
 		}
 
-		// Reset errors.
-		mEmailView.setError(null);
-		mPasswordView.setError(null);
+		urlbuffer.append("/api?name=").append(name).append("&method=")
+				.append(method).append("&username=").append(username)
+				.append("&password=").append(password);
 
-		// Store values at the time of the login attempt.
-		mEmail = mEmailView.getText().toString();
-		mPassword = mPasswordView.getText().toString();
+		url = urlbuffer.toString();
+		handler.setURL(url);		
+		requestHandler.running_flag = true;
+		handler.runrequest();
 
-		boolean cancel = false;
-		View focusView = null;
+		Boolean act = true;
+		while (act) {
+			if (!requestHandler.running_flag && !requestHandler.failed) {
+				Log.i("DEBUG", "Start handling loginrequest results and runningflag = " + requestHandler.running_flag);
+				
+				Map<String, String> JsonValues = requestHandler
+						.parseJson();
+				if (JsonValues.get("error").equals("false")
+						&& JsonValues.get("login_token").length() > 3) {
 
-		// Check for a valid password.
-		if (TextUtils.isEmpty(mPassword)) {
-			mPasswordView.setError(getString(R.string.error_field_required));
-			focusView = mPasswordView;
-			cancel = true;
-		} else if (mPassword.length() < 1) {
-			mPasswordView.setError(getString(R.string.error_invalid_password));
-			focusView = mPasswordView;
-			cancel = true;
-		} else if (mPassword.equals("oud")){
-			mPasswordView.setError(getString(R.string.error_old_password));
-			focusView = mPasswordView;
-			cancel = true;
-		}
-
-		// Check for a valid email address.
-		if (TextUtils.isEmpty(mEmail)) {
-			mEmailView.setError(getString(R.string.error_field_required));
-			focusView = mEmailView;
-			cancel = true;
-		}
-
-		if (cancel) {
-			// There was an error; don't attempt login and focus the first
-			// form field with an error.
-			focusView.requestFocus();
-		} else {
-			// Show a progress spinner, and kick off a background task to
-			// perform the user login attempt.
-			mLoginStatusMessageView.setText(R.string.login_progress_signing_in);
-			showProgress(true);
-			mAuthTask = new UserLoginTask();
-			mAuthTask.execute((Void) null);
-			Intent intent = new Intent(LoginActivity.this, MenuActivity.class);
-			LoginActivity.this.startActivity(intent);
-		}
-	}
-
-	/**
-	 * Shows the progress UI and hides the login form.
-	 */
-	@TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
-	private void showProgress(final boolean show) {
-		// On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
-		// for very easy animations. If available, use these APIs to fade-in
-		// the progress spinner.
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
-			int shortAnimTime = getResources().getInteger(
-					android.R.integer.config_shortAnimTime);
-
-			mLoginStatusView.setVisibility(View.VISIBLE);
-			mLoginStatusView.animate().setDuration(shortAnimTime)
-					.alpha(show ? 1 : 0)
-					.setListener(new AnimatorListenerAdapter() {
-						@Override
-						public void onAnimationEnd(Animator animation) {
-							mLoginStatusView.setVisibility(show ? View.VISIBLE
-									: View.GONE);
-						}
-					});
-
-			mLoginFormView.setVisibility(View.VISIBLE);
-			mLoginFormView.animate().setDuration(shortAnimTime)
-					.alpha(show ? 0 : 1)
-					.setListener(new AnimatorListenerAdapter() {
-						@Override
-						public void onAnimationEnd(Animator animation) {
-							mLoginFormView.setVisibility(show ? View.GONE
-									: View.VISIBLE);
-						}
-					});
-		} else {
-			// The ViewPropertyAnimator APIs are not available, so simply show
-			// and hide the relevant UI components.
-			mLoginStatusView.setVisibility(show ? View.VISIBLE : View.GONE);
-			mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-		}
-	}
-
-	/**
-	 * Represents an asynchronous login/registration task used to authenticate
-	 * the user.
-	 */
-	public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
-		@Override
-		protected Boolean doInBackground(Void... params) {
-			// TODO: attempt authentication against a network service.
-
-			try {
-				// Simulate network access.
-				Thread.sleep(2000);
-			} catch (InterruptedException e) {
-				return false;
-			}
-
-			for (String credential : DUMMY_CREDENTIALS) {
-				String[] pieces = credential.split(":");
-				if (pieces[0].equals(mEmail)) {
-					// Account exists, return true if the password matches.
-					return pieces[1].equals(mPassword);
+					requestHandler.token = JsonValues.get("login_token");
+					Log.i("Token", requestHandler.token);
+					Intent myIntent = new Intent(v.getContext(),
+							MenuActivity.class);
+					Toast.makeText(LoginActivity.this, "Login Successful",
+							Toast.LENGTH_LONG).show();
+					startActivityForResult(myIntent, 0);
+				} else if (JsonValues.get("error").equals("true")) {
+					Toast.makeText(LoginActivity.this,
+							JsonValues.get("message"), Toast.LENGTH_LONG)
+							.show();
+				} else {
+					Toast.makeText(LoginActivity.this, "Something went wrong",
+							Toast.LENGTH_LONG).show();
 				}
+
+				act = false;
+
+			}
+			else if(!requestHandler.running_flag && requestHandler.failed){
+				act = false;
+				Toast.makeText(LoginActivity.this, "Something went wrong",
+						Toast.LENGTH_LONG).show();
 			}
 
-			// TODO: register the new account here.
-			return true;
-		}
-
-		@Override
-		protected void onPostExecute(final Boolean success) {
-			mAuthTask = null;
-			showProgress(false);
-
-			if (success) {
-				finish();
-			} else {
-				mPasswordView
-						.setError(getString(R.string.error_incorrect_password));
-				mPasswordView.requestFocus();
-			}
-		}
-
-		@Override
-		protected void onCancelled() {
-			mAuthTask = null;
-			showProgress(false);
 		}
 	}
+
 }
---
+++ src/com/example/myhealthapp/requestHandler.java
@@ -0,0 +1,99 @@
+package com.example.myhealthapp;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import android.util.Base64;
+
+public class requestHandler {
+
+	static URI url = null;
+	static String host = "http://145.37.72.146:1234";
+	static String params = "";
+	static String response = "";
+	static Boolean running_flag = false;
+	static String token = "";
+	static boolean failed;
+
+	Thread thread = new Thread(new Runnable() {
+		@Override
+		public void run() {
+			try {
+				failed = false;
+				callWebService();
+			} catch (Exception e) {
+				failed = true;
+				e.printStackTrace();
+			} finally {
+				requestHandler.running_flag = false;
+				interupt();
+			}
+		}
+	});
+
+	public void runrequest() {
+		thread.start();
+	}
+
+	protected void interupt() {
+		thread.interrupt();
+	}
+	
+	public void setHost(String host) {
+		requestHandler.host = host;
+	}
+
+	public void setURL(String apiUrl) {
+		try {
+			requestHandler.url = new URI(apiUrl);
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+	}
+
+		public void setParams(String params) {
+		requestHandler.params = params;
+	}
+
+	public void callWebService() throws IOException {		
+		HttpClient httpclient = new DefaultHttpClient();
+		HttpResponse response = httpclient.execute(new HttpGet(
+				requestHandler.url));
+		StatusLine statusLine = response.getStatusLine();
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		response.getEntity().writeTo(out);
+		out.close();
+		requestHandler.response = out.toString();
+		if (statusLine.getStatusCode() != HttpStatus.SC_OK) {
+
+			response.getEntity().getContent().close();
+		}
+	}
+
+	public String encode(String value) throws UnsupportedEncodingException {
+		return Base64.encodeToString(value.getBytes("UTF-8"), Base64.NO_WRAP);
+	}
+
+	public static Map<String, String> parseJson() {
+		String[] splits = response.toString()
+				.substring(1, response.toString().length() - 1).split(",");
+		Map<String, String> JsonValues = new HashMap<String, String>();
+		for (String split : splits) {
+			JsonValues.put(split.split(":")[0].replaceAll("\"", ""),
+					split.split(":")[1].replaceAll("\"", ""));
+		}
+		return JsonValues;
+	}
+}
